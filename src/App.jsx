7fe2551e31ea169ce0c2f6510e819076bcb287f7
Bilal Benyoussef <bilalbeny735@gmail.com>
2024-09-26 21:56:46 +0200

    add functionaliçties to the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,86 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Square from './components/Square';
-
-const winningsPossibilities = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2],
-];
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { winningConditions } from './patterns';
 
 
 
 export default function App() {
 
 
-
   const [currentPlayer, setCurrentPlayer] = useState("X");
-
   const [board, setBoard] = useState(() => {
     return Array(9).fill(null)
   })
+
+  const [result, setResult] = useState({
+    winner: "",
+    state: "none",
+    winningCondition: []
+  })
+
+
+  useEffect(() => {
+    checkWinner()
+    checkDraw();
+  }, [board])
+
+  useEffect(() => {
+    if (result.state === "won") {
+      toast.dismiss()
+      toast.success(`Player ${result.winner} wins!`, { autoClose: 1000 })
+    }
+    if (result.state === "draw") {
+      toast.info("It's a draw!", { autoClose: 1000 });
+    }
+  }, [result])
+
+
+
+
+
+  const handleChooseSquareClick = (square) => {
+    if (board[square] || result.state === "won" || result.state === "draw") return;
+
+    setBoard(prevBoard =>
+      prevBoard.map((val, index) => (index === square ? currentPlayer : val))
+    );
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const checkWinner = () => {
+    winningConditions.forEach(condition => {
+      const [a, b, c] = condition;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setResult({
+          winner: board[a],
+          state: "won",
+          winningCondition: condition
+        });
+      }
+    });
+  };
+
+  const checkDraw = () => {
+    // If no winner and all squares are filled, it's a draw
+    if (!board.includes(null) && result.state === "none") {
+      setResult({
+        winner: "",
+        state: "draw",
+        winningCondition: []
+      });
+    }
+  };
+
+  const handleRestartGame = () => {
+    setBoard(Array(9).fill(null));
+    setCurrentPlayer("X");
+    setResult({ winner: "", state: "none", winningCondition: [] });
+    toast.dismiss()
+    toast.info("Game restarted", { autoClose: 500 });
+  };
+
 
 
 
@@ -31,27 +88,77 @@ export default function App() {
 
   return (
     <div className='grid min-h-screen sw-full place-content-center'>
+      <p className="fixed font-semibold -translate-x-1/2 top-2 left-1/2 badge badge-error badge-lg">{currentPlayer}'s Turn</p>
       <div className='flex flex-col items-center justify-center p-1 shadow-lg bg-base-300 rounded-box border-neutral'>
         <div className='flex -space-x-1.5 items-center'>
-          <Square chooseSquare={() => handleChooseClick(0)} player={board[0]} />
-          <Square chooseSquare={() => handleChooseClick(1)} player={board[1]} />
-          <Square chooseSquare={() => handleChooseClick(2)} player={board[2]} />
+          <Square
+            chooseSquare={() => handleChooseSquareClick(0)}
+            squareNumber={0}
+            player={board[0]}
+            result={result}
+            board={board}
+          />
+          <Square
+            chooseSquare={() => handleChooseSquareClick(1)}
+            squareNumber={1}
+            player={board[1]}
+            result={result}
+            board={board}
+          />
+          <Square
+            chooseSquare={() => handleChooseSquareClick(2)}
+            squareNumber={2}
+            player={board[2]}
+            result={result}
+            board={board}
+          />
         </div>
         <div className='flex -space-x-1.5 items-center'>
-          <Square chooseSquare={() => handleChooseClick(3)} player={board[3]} />
-          <Square chooseSquare={() => handleChooseClick(4)} player={board[4]} />
-          <Square chooseSquare={() => handleChooseClick(5)} player={board[5]} />
+          <Square chooseSquare={() => handleChooseSquareClick(3)}
+            squareNumber={3}
+            player={board[3]}
+            result={result}
+            board={board}
+          />
+          <Square chooseSquare={() => handleChooseSquareClick(4)}
+            squareNumber={4}
+            player={board[4]}
+            result={result}
+            board={board}
+          />
+          <Square chooseSquare={() => handleChooseSquareClick(5)}
+            squareNumber={5}
+            player={board[5]}
+            result={result}
+            board={board}
+          />
         </div>
         <div className='flex items-center -space-x-1.5'>
-          <Square chooseSquare={() => handleChooseClick(6)} player={board[6]} />
-          <Square chooseSquare={() => handleChooseClick(7)} player={board[7]} />
-          <Square chooseSquare={() => handleChooseClick(8)} player={board[8]} />
+          <Square chooseSquare={() => handleChooseSquareClick(6)}
+            squareNumber={6}
+            player={board[6]}
+            result={result}
+            board={board}
+          />
+          <Square chooseSquare={() => handleChooseSquareClick(7)}
+            squareNumber={7}
+            player={board[7]}
+            result={result}
+            board={board}
+          />
+          <Square chooseSquare={() => handleChooseSquareClick(8)}
+            squareNumber={8}
+            player={board[8]}
+            result={result}
+            board={board}
+          />
         </div>
 
       </div>
-      <div className='flex justify-center mt-4'>
-        <button type="Square" className='cursor-pointer btn btn-error btn-sm' >Restart</button>
+      <div className='flex flex-col items-center justify-center mt-4'>
+        <button type="button" disabled={board.every(indx => indx === null)} className='cursor-pointer btn btn-error btn-sm' onClick={handleRestartGame}>Restart</button>
       </div>
+      <ToastContainer />
     </div>
   )
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
-export default function Square({ player, chooseSquare }) {
+export default function Square({ player, chooseSquare, board, squareNumber, result }) {
     return (
-        <button type='button' className='w-[100px] disabled:scale-100 disabled:opacity-50 disabled:cursor-not-allowed h-[90px] mask mask-squircle flex items-center justify-center cursor-pointer hover:scale-[0.98] transition hover:opacity-80 bg-secondary' onClick={chooseSquare} >
+        <button
+            className={`w-[100px] h-[90px] mask mask-squircle flex items-center justify-center cursor-pointer transition bg-secondary ${result.state === "won" && result.winningCondition.includes(squareNumber) ? 'opacity-50' : 'opacity-100'} ${result.state !== "none" && !result.winningCondition.includes(squareNumber) ? 'cursor-not-allowed' : 'active:scale-[0.98] active:opacity-80'}`}
+            onClick={chooseSquare}
+            disabled={!!board[squareNumber] || result.state === "won" || result.state === "draw"}
+        >
             {
                 player === "X" && <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/X_mark.svg/1792px-X_mark.svg.png" className='w-[40px] h-[40px]' alt="x-mark" />
             }
             {
                 player === "O" && <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Letter_o.svg/1200px-Letter_o.svg.png" className='w-[35px]' alt="o-letter" />
             }
+
         </button>
     )
 }
+
+
